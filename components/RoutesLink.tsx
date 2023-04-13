452cd6fc9b8188b11880f172782be3b5ef84b0d0
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-04-13 11:04:38 +0800

    Update RoutesLink.tsx

--- components/RoutesLink.tsx
+++ components/RoutesLink.tsx
@@ -7,12 +7,12 @@ const RoutesLink = () => {
     return (
         <>
             <Link href="/" className={router.pathname === "/" ? "active" : ""}>回到首頁</Link>
-            <Link href="/nextjs_works" className={router.pathname === "/nextjs_works" ? "active" : ""}>Next.js做了什麼</Link>
-            <Link href="/nextjs_render" className={router.pathname === "/nextjs_render" ? "active" : ""}>Next.js渲染模式</Link>
-            <Link href="/nextjs_styles" className={router.pathname === "/nextjs_styles" ? "active" : ""}>Next.js中的樣式</Link>
-            <Link href="/nextjs_routes" className={router.pathname === "/nextjs_routes" ? "active" : ""}>自動化的路由</Link>
-            <Link href="/nextjs_image" className={router.pathname === "/nextjs_image" ? "active" : ""}>Next.js中的圖片</Link>
-            <Link href="/nextjs_api" className={router.pathname === "/nextjs_api" ? "active" : ""}>Next.js中的API</Link>
+            <Link href="/Nextjs_works" className={router.pathname === "/Nextjs_works" ? "active" : ""}>Next.js做了什麼</Link>
+            <Link href="/Nextjs_render" className={router.pathname === "/Nextjs_render" ? "active" : ""}>Next.js渲染模式</Link>
+            <Link href="/Nextjs_styles" className={router.pathname === "/Nextjs_styles" ? "active" : ""}>Next.js中的樣式</Link>
+            <Link href="/Nextjs_routes" className={router.pathname === "/Nextjs_routes" ? "active" : ""}>自動化的路由</Link>
+            <Link href="/Nextjs_image" className={router.pathname === "/Nextjs_image" ? "active" : ""}>Next.js中的圖片</Link>
+            <Link href="/Nextjs_api" className={router.pathname === "/Nextjs_api" ? "active" : ""}>Next.js中的API</Link>
         </>
     );
 };
